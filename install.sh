b928ee51f067e978069bf3f91e77e9c9ef0a3874
Mr.Dung <dung13890@gmail.com>
2017-08-16 07:58:03 +0700

    Change permission docker

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 # Install Laravel
 composer create-project --prefer-dist laravel/laravel blog
 
-# Move all content to root
+# Move all content to root && remove README.md
+rm -rf README.md
 mv blog/* blog/.[^.]* . && rmdir blog/
 
 # Chown www-data
-chown -R www-data:www-data * .*
+chown -R www-data:1000 * .*
 
